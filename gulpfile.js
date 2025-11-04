b9c89194b0543b498843e959777b69a435bdb8a5
gimorais <gimorais.contato@gmail.com>
2025-11-04 14:31:09 -0300

    feat: adiciona seção Hero com vídeo de fundo e botão animado

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+// Compilar SCSS
 function styles() {
     return gulp.src('src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+// Copiar imagens
+function images() {
+    return gulp.src('src/images/**/*')
+        .pipe(gulp.dest('dist/images'));
+}
+
+// Tarefas exportadas
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('src/styles/*.scss', gulp.parallel(styles));
-}
+};
