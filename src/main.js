bd0d5e28551481618b309a3b93c961a4ef12dc08
gimorais <gimorais.contato@gmail.com>
2025-11-05 19:52:02 -0300

    Corrigido script das abas e melhorada lógica do forEach

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
         const targetId = button.dataset.tabButton; // exemplo: "livros" ou "filmes"
         const targetTab = document.querySelector(`[data-tab-id="${targetId}"]`);
 
-        // 1️⃣ remove o ativo de todos os botões e abas
+        // 1 remove o ativo de todos os botões e abas
         buttons.forEach(b => b.classList.remove('midia__tabs__button--is-active'));
         tabs.forEach(t => t.classList.remove('midia__list--is-active'));
 
-        // 2️⃣ ativa apenas o botão e a aba clicados
+        // 2 ativa apenas o botão e a aba clicados
         button.classList.add('midia__tabs__button--is-active');
         targetTab.classList.add('midia__list--is-active');
         });
